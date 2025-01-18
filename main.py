1094b000f060fa52fd2bf26e2df8f5bbcdfaff9a
QuixoticZ <zwcontact@gmail.com>
2025-01-18 03:17:39 -0500

    report structure added

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ def main():
     path = "books/frankenstein.txt"
     full_text = get_text(path) 
     num_words = word_count(full_text)
+    report_details = char_report(letter_count(full_text))
     print(f"--- Begin report of {path} ---")
     print(f"{num_words} words found in the document")
-    print (letter_count(full_text))
-    
+    for c in report_details:
+        print(f"The '{c["char"]}' character was found {c["count"]} times")
+    print("--- End Report ---")   
     
 def get_text(path):
     with open(path) as f:
@@ -24,8 +26,18 @@ def letter_count(text):
             char_dict[l.lower()] +=1
     return char_dict
 
+def sort_on(dict):
+    return dict["count"]
+
 def char_report(char_dict):
-    pass
+    list_dict=[]
+    for i in char_dict:
+            if i.isalpha() == True:
+                char = char_dict[i]
+                list_dict.append({"char":i,"count":char})
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
+
 
 
 main()
